19d96dd94358027dfdb20a0147d594c954d71faf
Your Name <retroam@yahoo.com>
2015-02-04 13:38:03 -0500

    test api calls

--- test.py
+++ test.py
@@ -1,17 +1,44 @@
 from requests_oauthlib import OAuth2Session
-import requests
+import json
 
-CLIENT_ID = '5798d0b0-4d94-4d74-b029-7235c4369016'
-CLIENT_SECRET = 'a59ce62c-2006-4996-9e43-923e091d349a'
-REDIRECT_URI = "www.smartthings.com"
+
+token = ''
+endpoint = ''
+CLIENT_ID = '4f3928af-c380-4deb-9ff0-1a035d36e3ba'
+CLIENT_SECRET = 'ae30a8ee-c9b4-4f38-9a77-6aff576f3ac3'
+REDIRECT_URI = "http://127.0.0.1:5000/callback"
 SCOPE = ["app"]
-OAUTH_AUTHORIZE_URL = "https://graph.api.smartthings.com/oauth/authorize"
-OAUTH_ACCESS_TOKEN_URL = "https://graph.api.smartthings.com/oauth/www.smartthings.com"
 
 oauth_session = OAuth2Session(CLIENT_ID,
                               redirect_uri=REDIRECT_URI,
                               scope=SCOPE)
 
-authorization_url, state = oauth_session.authorization_url(OAUTH_AUTHORIZE_URL)
-print 'Please go here and authorize,', authorization_url
-redirect_response = raw_input('Paste the full redirect URL here: ')
+devices_header = {
+            "Authorization": "Bearer %s" % token,
+        }
+
+# Locks
+# Supported commands: [lock, unlock]
+print oauth_session.put(endpoint + '/lock',
+                        headers=devices_header,
+                        data=json.dumps({'command': 'unlock'})
+                         )
+print oauth_session.get(endpoint + '/lock',
+                        headers=devices_header
+                        ).json()
+
+
+# Switch
+# Supported commands: [on, off]
+r = oauth_session.get(endpoint + '/switch',
+                       headers=devices_header
+                      ).json()
+device_id = r[0]['id']
+print oauth_session.put(endpoint + '/switch' + '/' + device_id,
+                      headers=devices_header,
+                      data=json.dumps({'command': 'off'})
+                      )
+print device_id
+print oauth_session.get(endpoint + '/switch' + '/' +  device_id,
+                       headers=devices_header
+                      ).json()
